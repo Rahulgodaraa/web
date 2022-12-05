34ffc654158fd2107dda676e101b94aa175587f3
Rahulgodaraa <rgodara152001@gmail.com>
2022-12-05 21:35:38 +0530

    New commite

---
+++ javascript/ch5.js
@@ -0,0 +1,22 @@
+//type convertion
+{
+let rollnumber = 22;
+console.log(rollnumber , typeof(rollnumber))
+}
+
+let number = 02;
+number = String(02);
+console.log(number , typeof(number))
+
+
+//array to string
+{
+let arr = [20,20,2,0,2,0,2,0]
+console.log(length.arr , typeof(arr))
+}
+
+{
+let array = [60,60,6,06,60,6,60]
+array= String([60,60,6,06,60,6,60])
+console.log(length.array , typeof(array))
+}
